d4d232ce1c47fcf2f439c260634963fef322d0c5
VinoliaBattiri <vinoliabattiri@gmail.com>
2024-07-11 20:18:26 -0400

    Time: 4 ms (62.71%), Space: 44.4 MB (88.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,16 @@
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i<nums.length; i++){
-            for(int j =i+1; j<nums.length; j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[] {i,j};
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (map.containsKey(difference) && map.get(difference) != i) {
+                return new int[] { i, map.get(difference) };
             }
         }
-        return new int[]{};
+        // In case there is no solution, we'll just return null
+        return null;
     }
 }
-
